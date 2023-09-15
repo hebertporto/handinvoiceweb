237982d03ce99de5ec117e087625bc707414fc8f
Hebert Porto <hebertporto@gmail.com>
2023-09-15 17:09:39 -0400

    adding preview to the invoice

--- app/[lang]/dashboard/page.tsx
+++ app/[lang]/dashboard/page.tsx
@@ -45,7 +45,19 @@ export default function DashboardPage() {
     console.log("alow")
     console.log(data)
   }
+  const watchedFields = watch([
+    "date",
+    "invoiceNumber",
+    "from",
+    "billTo",
+    "items",
+    "taxRate",
+    "terms",
+    "paymentDetails",
+    "logo",
+  ])
 
+  console.log("watchedFields", watchedFields)
   // const session = await getServerSession()
   // if (!session || !session.user) {
   //   redirect("/api/auth/signin")
@@ -72,17 +84,37 @@ export default function DashboardPage() {
             className="flex flex-col gap-6"
           >
             {/* Date */}
-            <label htmlFor="date">Date:</label>
-            <input type="date" {...register("date", { required: true })} />
-            {errors.date && <span>Date is required</span>}
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="date" className="font-medium">
+                Date:
+              </label>
+              <input
+                type="date"
+                {...register("date", { required: true })}
+                className="rounded border p-2"
+              />
+              {errors.date && (
+                <span className="text-sm text-red-500">Date is required</span>
+              )}
+            </div>
 
             {/* Invoice Number */}
-            <label htmlFor="invoiceNumber">Invoice Number:</label>
-            <input
-              type="text"
-              {...register("invoiceNumber", { required: true })}
-            />
-            {errors.invoiceNumber && <span>Invoice Number is required</span>}
+            <div className="flex flex-col gap-2">
+              <label htmlFor="date" className="font-medium">
+                Invoice Number:
+              </label>
+              <input
+                type="text"
+                {...register("invoiceNumber", { required: true })}
+                className="rounded border p-2"
+              />
+              {errors.invoiceNumber && (
+                <span className="text-sm text-red-500">
+                  Invoice Number is required
+                </span>
+              )}
+            </div>
 
             {/* From */}
             <label htmlFor="from">From:</label>
@@ -95,30 +127,38 @@ export default function DashboardPage() {
             {errors.billTo && <span>Recipient details are required</span>}
 
             {fields.map((field, index) => (
-              <div key={index} className="flex items-center gap-4">
-                <label>Description:</label>
-
-                <input
-                  {...register(`items[${index}].description` as any)}
-                  defaultValue={field.description}
-                />
-
-                <label>Hours:</label>
-
-                <input
-                  type="number"
-                  {...register(`items[${index}].hours` as any)}
-                  defaultValue={field.hours}
-                />
-
-                <label>Rate:</label>
-
-                <input
-                  type="number"
-                  {...register(`items[${index}].rate` as any)}
-                  defaultValue={field.rate}
-                />
-                <button type="button" onClick={() => remove(index)}>
+              <div key={index} className="grid grid-cols-3 items-center gap-4">
+                <div className="col-span-1">
+                  <label>Description:</label>
+                  <input
+                    {...register(`items[${index}].description` as any)}
+                    defaultValue={field.description}
+                    className="w-full rounded border p-2"
+                  />
+                </div>
+                <div className="col-span-1">
+                  <label>Hours:</label>
+                  <input
+                    type="number"
+                    {...register(`items[${index}].hours` as any)}
+                    defaultValue={field.hours}
+                    className="w-full rounded border p-2"
+                  />
+                </div>
+                <div className="col-span-1 flex-col items-center">
+                  <label className="mr-2">Rate:</label>
+                  <input
+                    type="number"
+                    {...register(`items[${index}].rate` as any)}
+                    defaultValue={field.rate}
+                    className="w-full rounded border p-2"
+                  />
+                </div>
+                <button
+                  type="button"
+                  onClick={() => remove(index)}
+                  className="ml-2 text-red-500"
+                >
                   Remove
                 </button>
               </div>
@@ -126,10 +166,8 @@ export default function DashboardPage() {
 
             <Button
               type="button"
-              onClick={() => {
-                console.log("append")
-                append({ description: "", hours: 0, rate: 0 })
-              }}
+              onClick={() => append({ description: "", hours: 0, rate: 0 })}
+              className="mt-4"
             >
               Add Item
             </Button>
@@ -154,9 +192,65 @@ export default function DashboardPage() {
         </div>
 
         {/* Right Column */}
-        <div className="w-1/2 bg-white p-8 shadow-md">
-          {/* Invoice Preview goes here */}
-          {/* ... */}
+
+        <div className="w-1/2 rounded-lg border bg-white p-8 shadow-md">
+          <div className="mb-6 flex items-center justify-between">
+            <h2 className="text-2xl font-bold">Invoice Preview</h2>
+            {/* Uncomment and use when you integrate the logo upload */}
+            {/* {watchedFields[8] && <img src={URL.createObjectURL(watchedFields[8][0])} alt="Uploaded logo" className="h-16 w-auto" />} */}
+          </div>
+
+          <div className="mb-6">
+            <p className="text-sm font-semibold">Date: {watchedFields[0]}</p>
+            <p className="text-sm font-semibold">
+              Invoice Number: {watchedFields[1]}
+            </p>
+          </div>
+
+          <div className="mb-6 grid grid-cols-2 gap-6">
+            <div>
+              <p className="text-sm font-semibold">From:</p>
+              <p className="text-sm">{watchedFields[2]}</p>
+            </div>
+            <div>
+              <p className="text-sm font-semibold">Bill To:</p>
+              <p className="text-sm">{watchedFields[3]}</p>
+            </div>
+          </div>
+
+          <div className="mb-6">
+            <table className="w-full border-collapse">
+              <thead>
+                <tr className="border-b-2">
+                  <th className="px-4 py-2 text-left">Description</th>
+                  <th className="px-4 py-2 text-right">Hours</th>
+                  <th className="px-4 py-2 text-right">Rate</th>
+                  <th className="px-4 py-2 text-right">Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {watchedFields[4] &&
+                  watchedFields[4].map((item, index) => (
+                    <tr key={index} className="border-b">
+                      <td className="px-4 py-2">{item.description}</td>
+                      <td className="px-4 py-2 text-right">{item.hours}</td>
+                      <td className="px-4 py-2 text-right">{item.rate}</td>
+                      <td className="px-4 py-2 text-right">
+                        {item.hours * item.rate}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+
+          <div className="mb-6">
+            <p className="text-sm">Tax Rate: {watchedFields[5]}%</p>
+            <p className="text-sm">Terms: {watchedFields[6]}</p>
+            <p className="text-sm">Payment Details: {watchedFields[7]}</p>
+          </div>
+
+          {/* Add any additional details or calculations */}
         </div>
       </div>
     </section>
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -6,7 +6,7 @@ import LanguageSwitcher from "./LangueSwitcher"
 
 export function SiteHeader({ lang }: { lang: Locale }) {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
+    <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav lang={lang} />
         <div className="flex flex-1 items-center justify-end space-x-4">
