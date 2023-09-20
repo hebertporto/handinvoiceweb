b08d91ae7d79bf63f0ddfd0e3b457f186924b8e6
Alfredo Gasperi <eu@alfred.ooo>
2023-09-20 17:37:08 -0300

    Step by step form

--- app/[lang]/dashboard/page.tsx
+++ app/[lang]/dashboard/page.tsx
@@ -1,60 +1,108 @@
 import { redirect } from "next/navigation"
 import { getServerSession } from "next-auth"
+import { Button } from "@/components/Button"
+
+const invoices = [
+  { date: '2023-09-15', number: 'INV-000123', to: 'Jhon Doe', total: '$1,2000.00' },
+  // More invoice...
+]
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default async function DashboardPage() {
   const session = await getServerSession()
   if (!session || !session.user) {
     redirect("/api/auth/signin")
   }
+
   return (
-    <section className="container mx-auto grid items-center gap-6 px-4 pb-8 pt-6 md:py-10">
-      <div className="flex items-center justify-between">
-        {/* Search Bar */}
-        <input
-          type="text"
-          placeholder="Search Invoices"
-          className="w-1/2 rounded-md border p-2"
-        />
+    <div className="mx-auto max-w-full px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <h1 className="text-base font-semibold leading-6 text-gray-900">Invoices</h1>
+            <p className="mt-1 text-sm text-gray-700">You have a total of <strong className="text-purple-800">1 invoice(s) </strong>. Try to use the search bar to find what you need.</p>
+          </div>
+          <div className="mt-4 flex items-center space-x-3 sm:ml-16 sm:mt-0 sm:flex-none">
+            {/* SEARCH */}
+            <div className="relative mt-1 flex items-center">
+              <input
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Type here to search..."
+                className="block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+                <kbd className="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-400">
+                  ⌘K
+                </kbd>
+              </div>
+            </div>
 
-        {/* Add Invoice Button */}
-
-        <a
-          href="/invoice"
-          className="rounded-full bg-blue-500 px-4 py-2 font-semibold text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
-        >
-          Add Invoice
-        </a>
+            {/* ADD BUTTON */}
+            <Button variant="solid" color="blue" type="button" href={'/invoice'}> 
+              Add Invoice 
+            </Button>
+          </div>
+        </div>
+        <div className="mx-auto my-4 w-full border-y border-gray-200">
+          <div className="flex items-center space-x-4 py-4">
+            <button
+              type="button"
+              className="rounded-md bg-white p-3 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            >
+              All Invoces <span className="ml-2 rounded-md bg-blue-100 px-2 py-1 text-sm">12</span>
+            </button>
+            <button
+              type="button"
+              className="rounded-md bg-white p-3 text-sm font-semibold text-gray-900 hover:bg-gray-50"
+            >
+              Pending <span className="ml-2 rounded-md bg-orange-100 px-2 py-1 text-sm">6</span>
+            </button>
+            <button
+              type="button"
+              className="rounded-md bg-white p-3 text-sm font-semibold text-gray-900 hover:bg-gray-50"
+            >
+              Sent <span className="ml-2 rounded-md bg-emerald-100 px-2 py-1 text-sm">3</span>
+            </button>
+            <button
+              type="button"
+              className="rounded-md bg-white p-3 text-sm font-semibold text-gray-900 hover:bg-gray-50"
+            >
+              Error <span className="ml-2 rounded-md bg-rose-100 px-2 py-1 text-sm">3</span>
+            </button>
+          </div>
+        </div>
+        <div className="-mx-4 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg">
+          <table className="min-w-full divide-y divide-gray-300">
+            <thead>
+              <tr>
+                <th className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Date</th>
+                <th className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"># Number</th>
+                <th className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">To</th>
+                <th className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Total</th>
+                <th className="relative py-3.5 pl-3 pr-4 sm:pr-6"><span className="sr-only">Edit</span></th>
+              </tr>
+            </thead>
+            <tbody>
+              {invoices.map((invoice, invoiceIdx) => (
+                <tr key={invoice.number}>
+                  <td className={classNames(invoiceIdx === 0 ? '' : 'border-t border-transparent', 'relative py-4 pl-4 pr-3 text-sm sm:pl-6')}>{invoice.date}</td>
+                  <td className={classNames(invoiceIdx === 0 ? '' : 'border-t border-gray-200', 'px-3 py-4 text-sm text-gray-500')}>{invoice.number}</td>
+                  <td className={classNames(invoiceIdx === 0 ? '' : 'border-t border-gray-200', 'px-3 py-4 text-sm text-gray-500')}>{invoice.to}</td>
+                  <td className={classNames(invoiceIdx === 0 ? '' : 'border-t border-gray-200', 'px-3 py-4 text-sm text-gray-500')}>{invoice.total}</td>
+                  <td className={classNames(invoiceIdx === 0 ? '' : 'border-t border-transparent', 'relative py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6')}>
+                    <Button variant="outline" color="slate" type="button" href={'/invoice'}>Edit</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-
-      {/* Invoice Table */}
-      <table className="min-w-full overflow-hidden rounded-md bg-white">
-        <thead>
-          <tr>
-            <th className="border-b border-gray-300 px-4 py-2 text-left text-sm font-semibold uppercase tracking-wider text-gray-600">
-              Date
-            </th>
-            <th className="border-b border-gray-300 px-4 py-2 text-left text-sm font-semibold uppercase tracking-wider text-gray-600">
-              Invoice Number
-            </th>
-            <th className="border-b border-gray-300 px-4 py-2 text-left text-sm font-semibold uppercase tracking-wider text-gray-600">
-              To
-            </th>
-            <th className="border-b border-gray-300 px-4 py-2 text-left text-sm font-semibold uppercase tracking-wider text-gray-600">
-              Total
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* Example row, you can map your invoice data here */}
-          <tr className="transition duration-150 ease-in-out hover:bg-gray-100">
-            <td className="border-b border-gray-300 px-4 py-2">2023-09-15</td>
-            <td className="border-b border-gray-300 px-4 py-2">INV-00123</td>
-            <td className="border-b border-gray-300 px-4 py-2">John Doe</td>
-            <td className="border-b border-gray-300 px-4 py-2">$1200.00</td>
-          </tr>
-          {/* ... more rows */}
-        </tbody>
-      </table>
-    </section>
+    </div>
   )
 }
--- app/[lang]/invoice/page.tsx
+++ app/[lang]/invoice/page.tsx
@@ -1,53 +1,58 @@
-"use client"
+'use client'
 
-import { redirect } from "next/navigation"
-import { useSession } from "next-auth/react"
-import { SubmitHandler, useFieldArray, useForm } from "react-hook-form"
+import React, { useState } from 'react';
+import { useSession } from "next-auth/react";
+import { SubmitHandler, useFieldArray, useForm } from "react-hook-form";
 
-import { Button } from "@/components/ui/button"
-
-import { generateStyledInvoice } from "./utils/pdfManager"
+import { Button } from "@/components/Button";
+import { generateStyledInvoice } from "./utils/pdfManager";
+import { redirect } from 'next/navigation';
 
 type ItemInput = {
-  description: string
-  hours: number
-  ratePerHour: number
-}
+  description: string;
+  hours: number;
+  ratePerHour: number;
+};
 
 type Inputs = {
-  date: string
-  invoiceNumber: string
-  from: string
-  billTo: string
-  items: ItemInput[]
-  taxRate: number
-  terms: string
-  paymentDetails: string
-  logo: FileList
-}
+  date: string;
+  invoiceNumber: string;
+  from: string;
+  billTo: string;
+  items: ItemInput[];
+  taxRate: number;
+  terms: string;
+  paymentDetails: string;
+  logo: FileList;
+};
 
 export default function InvoicePage() {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
+
   if (!session || !session.user) {
-    redirect("/api/auth/signin")
+    redirect("/api/auth/signin");
   }
+
   const {
     register,
     handleSubmit,
     watch,
     control,
     formState: { errors },
-  } = useForm<Inputs>()
+  } = useForm<Inputs>();
 
   const { fields, append, remove } = useFieldArray({
     control,
     name: "items",
-  })
+  });
+
+  const [step, setStep] = useState(1);
 
   const onSubmit: SubmitHandler<Inputs> = (data, e) => {
-    generateStyledInvoice(data)
-    e?.preventDefault()
-  }
+    generateStyledInvoice(data);
+    e?.preventDefault();
+  };
+
   const watchedFields = watch([
     "date",
     "invoiceNumber",
@@ -58,216 +63,160 @@ export default function InvoicePage() {
     "terms",
     "paymentDetails",
     "logo",
-  ])
+  ]);
 
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex gap-2 bg-gray-100">
-        <div className="w-1/2 p-8">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col gap-6"
-          >
-            <div className="flex flex-row justify-between gap-4">
-              <div className="flex flex-1 flex-col gap-2">
-                <label htmlFor="date" className="font-medium">
-                  Date:
-                </label>
-                <input
-                  type="date"
-                  {...register("date", { required: true })}
-                  className="rounded border p-2"
-                />
-                {errors.date && (
-                  <span className="text-sm text-red-500">Date is required</span>
-                )}
-              </div>
-
-              <div className="flex flex-1 flex-col gap-2">
-                <label htmlFor="invoiceNumber" className="font-medium">
-                  Invoice Number:
-                </label>
-                <input
-                  type="text"
-                  {...register("invoiceNumber", { required: true })}
-                  className="rounded border p-2"
-                />
-                {errors.invoiceNumber && (
-                  <span className="text-sm text-red-500">
-                    Invoice Number is required
-                  </span>
-                )}
-              </div>
-            </div>
-
-            <div className="flex flex-row justify-between gap-4">
-              <div className="flex flex-1 flex-col gap-2">
-                <label htmlFor="from">From:</label>
-                <textarea {...register("from", { required: true })} />
-                {errors.from && <span>Send details are required</span>}
-              </div>
-              <div className="flex flex-1 flex-col gap-2">
-                <label htmlFor="billTo">Bill To:</label>
-                <textarea {...register("billTo", { required: true })} />
-                {errors.billTo && <span>Recipient details are required</span>}
-              </div>
-            </div>
-
-            {fields.map((field, index) => (
-              <div key={index} className="grid grid-cols-6 items-center gap-4">
-                {/* Description field */}
-                <div className="col-span-3">
-                  <label>Description:</label>
-                  <input
-                    {...register(`items[${index}].description` as any)}
-                    defaultValue={field.description}
-                    className="w-full rounded border p-2"
-                  />
-                </div>
-
-                {/* Hours field */}
-                <div className="col-span-1">
-                  <label>Hours:</label>
-                  <input
-                    type="number"
-                    {...register(`items[${index}].hours` as any)}
-                    defaultValue={field.hours}
-                    className="w-full rounded border p-2"
-                  />
-                </div>
-
-                {/* Rate field */}
-                <div className="col-span-1">
-                  <label>Rate:</label>
-                  <input
-                    type="number"
-                    {...register(`items[${index}].ratePerHour` as any)}
-                    defaultValue={field.ratePerHour}
-                    className="w-full rounded border p-2"
-                  />
-                </div>
-
-                {/* Remove button */}
-                <div className="col-span-1">
-                  <button
-                    type="button"
-                    onClick={() => remove(index)}
-                    className="rounded p-2 text-red-500 hover:bg-red-100 focus:outline-none"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M6 2a2 2 0 012-2h4a2 2 0 012 2v1h5a1 1 0 011 1v1a1 1 0 01-1 1v10a2 2 0 01-2 2H4a2 2 0 01-2-2V6a1 1 0 01-1-1V4a1 1 0 011-1h5V2zm1 0h6v1H7V2z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            ))}
-
-            <Button
-              type="button"
-              onClick={() =>
-                append({ description: "", hours: 0, ratePerHour: 0 })
-              }
-              className="mt-4 rounded-full bg-blue-500 px-4 py-2 font-semibold text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            >
-              Add Item
-            </Button>
-
-            <label htmlFor="taxRate">Tax Rate:</label>
-            <input type="number" {...register("taxRate", { required: true })} />
-            {errors.taxRate && <span>Tax Rate is required</span>}
-
-            <label htmlFor="terms">Terms:</label>
-            <textarea {...register("terms", { required: true })} />
-            {errors.terms && <span>Payment terms are required</span>}
-
-            <label htmlFor="paymentDetails">Payment Details:</label>
-            <textarea {...register("paymentDetails", { required: true })} />
-            {errors.paymentDetails && <span>Payment details are required</span>}
-
-            {/* <label htmlFor="logo">Logo (Optional):</label>
-            <input type="file" {...register("logo")} /> */}
-
-            <Button
-              className="mt-4 rounded-full bg-blue-500 px-4 py-2 font-semibold text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
-              type="submit"
-            >
-              Submit
-            </Button>
-          </form>
-        </div>
-
-        {/* Right Column */}
-
-        <div className="w-1/2 rounded-lg border bg-white p-8 shadow-md">
-          <div className="mb-6 flex items-center justify-between">
-            <h2 className="text-2xl font-bold">Invoice Preview</h2>
-            {/* Uncomment and use when you integrate the logo upload */}
-            {/* {watchedFields[8] && <img src={URL.createObjectURL(watchedFields[8][0])} alt="Uploaded logo" className="h-16 w-auto" />} */}
-          </div>
-
-          <div className="mb-6">
-            <p className="text-sm font-semibold">Date: {watchedFields[0]}</p>
-            <p className="text-sm font-semibold">
-              Invoice Number: {watchedFields[1]}
+    <div className="mx-auto max-w-full px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <h1 className="text-base font-semibold leading-6 text-gray-900">Create Invoice</h1>
+            <p className="mt-1 text-sm text-gray-700">
+              Create your <strong className="text-purple-800">new</strong> invoice.
             </p>
           </div>
+        </div>
 
-          <div className="mb-6 grid grid-cols-2 gap-6">
-            <div>
-              <p className="text-sm font-semibold">From:</p>
-              <p className="text-sm">{watchedFields[2]}</p>
-            </div>
-            <div>
-              <p className="text-sm font-semibold">Bill To:</p>
-              <p className="text-sm">{watchedFields[3]}</p>
-            </div>
-          </div>
+        <div className="mt-4 flex gap-2 bg-gray-100">
+          <div className="w-1/2 p-8">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
+              {step === 1 && (
+                <>
+                  <div className="flex flex-row justify-between gap-4">
+                    <div className="flex flex-1 flex-col gap-2">
+                      <label htmlFor="date" className="font-medium">Date:</label>
+                      <input type="date" {...register("date", { required: true })} className="rounded border p-2" />
+                      {errors.date && <span className="text-sm text-red-500">Date is required</span>}
+                    </div>
 
-          <div className="mb-6">
-            <table className="w-full border-collapse">
-              <thead>
-                <tr className="border-b-2">
-                  <th className="px-4 py-2 text-left">Description</th>
-                  <th className="px-4 py-2 text-right">Hours</th>
-                  <th className="px-4 py-2 text-right">Rate</th>
-                  <th className="px-4 py-2 text-right">Amount</th>
-                </tr>
-              </thead>
-              <tbody>
-                {watchedFields[4] &&
-                  watchedFields[4].map((item, index) => (
-                    <tr key={index} className="border-b">
-                      <td className="px-4 py-2">{item.description}</td>
-                      <td className="px-4 py-2 text-right">{item.hours}</td>
-                      <td className="px-4 py-2 text-right">
-                        {item.ratePerHour}
-                      </td>
-                      <td className="px-4 py-2 text-right">
-                        {item.hours * item.ratePerHour}
-                      </td>
-                    </tr>
+                    <div className="flex flex-1 flex-col gap-2">
+                      <label htmlFor="invoiceNumber" className="font-medium">Invoice Number:</label>
+                      <input type="text" {...register("invoiceNumber", { required: true })} className="rounded border p-2" />
+                      {errors.invoiceNumber && <span className="text-sm text-red-500">Invoice Number is required</span>}
+                    </div>
+                  </div>
+
+                  <Button variant="solid" color="blue" type="button" onClick={() => setStep(2)}>
+                    Next
+                  </Button>
+                </>
+              )}
+
+              {step === 2 && (
+                <>
+                  <div className="flex flex-row justify-between gap-4">
+                    <div className="flex flex-1 flex-col gap-2">
+                      <label htmlFor="from">From:</label>
+                      <textarea {...register("from", { required: true })} />
+                      {errors.from && <span>Send details are required</span>}
+                    </div>
+                    <div className="flex flex-1 flex-col gap-2">
+                      <label htmlFor="billTo">Bill To:</label>
+                      <textarea {...register("billTo", { required: true })} />
+                      {errors.billTo && <span>Recipient details are required</span>}
+                    </div>
+                  </div>
+
+                  <Button variant="solid" color="blue" type="button" onClick={() => setStep(3)}>
+                    Next
+                  </Button>
+                </>
+              )}
+
+              {step === 3 && (
+                <>
+                  {fields.map((field, index) => (
+                    <div key={index} className="grid grid-cols-6 items-center gap-4">
+                      {/* Description field */}
+                      <div className="col-span-3">
+                        <label>Description:</label>
+                        <input {...register(`items[${index}].description` as any)} defaultValue={field.description} className="w-full rounded border p-2" />
+                      </div>
+
+                      {/* Hours field */}
+                      <div className="col-span-1">
+                        <label>Hours:</label>
+                        <input type="number" {...register(`items[${index}].hours` as any)} defaultValue={field.hours} className="w-full rounded border p-2" />
+                      </div>
+
+                      {/* Rate field */}
+                      <div className="col-span-1">
+                        <label>Rate:</label>
+                        <input type="number" {...register(`items[${index}].ratePerHour` as any)} defaultValue={field.ratePerHour} className="w-full rounded border p-2" />
+                      </div>
+
+                      {/* Remove button */}
+                      <div className="col-span-1">
+                        <button type="button" onClick={() => remove(index)} className="rounded p-2 text-red-500 hover:bg-red-100 focus:outline-none">
+                          Remove
+                        </button>
+                      </div>
+                    </div>
                   ))}
-              </tbody>
-            </table>
+
+                  <Button type="button" onClick={() => append({ description: "", hours: 0, ratePerHour: 0 })}>
+                    Add Item
+                  </Button>
+
+                  <Button variant="solid" color="blue" type="button" onClick={() => setStep(4)}>
+                    Next
+                  </Button>
+                </>
+              )}
+
+              {step === 4 && (
+                <>
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="taxRate" className="font-medium">Tax Rate:</label>
+                    <input type="text" {...register("taxRate", { required: true })} className="rounded border p-2" />
+                    {errors.taxRate && <span className="text-sm text-red-500">Tax Rate is required</span>}
+                  </div>
+
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="terms" className="font-medium">Terms:</label>
+                    <input type="text" {...register("terms", { required: true })} className="rounded border p-2" />
+                    {errors.terms && <span className="text-sm text-red-500">Terms are required</span>}
+                  </div>
+
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="paymentDetails" className="font-medium">Payment Details:</label>
+                    <input type="text" {...register("paymentDetails", { required: true })} className="rounded border p-2" />
+                    {errors.paymentDetails && <span className="text-sm text-red-500">Payment Details are required</span>}
+                  </div>
+
+                  <Button variant="solid" color="blue" type="submit">
+                    Create Invoice
+                  </Button>
+                </>
+              )}
+            </form>
           </div>
 
-          <div className="mb-6">
-            <p className="text-sm">Tax Rate: {watchedFields[5]}%</p>
-            <p className="text-sm">Terms: {watchedFields[6]}</p>
-            <p className="text-sm">Payment Details: {watchedFields[7]}</p>
-          </div>
+          {/* Right Column - Preview */}
+          <div className="w-1/2 p-8">
+            <h2>Preview</h2>
+            <div>
+              <p><strong>Date:</strong> {watchedFields[0]}</p>
+              <p><strong>Invoice Number:</strong> {watchedFields[1]}</p>
+              <p><strong>From:</strong> {watchedFields[2]}</p>
+              <p><strong>Bill To:</strong> {watchedFields[3]}</p>
+
+              {watchedFields[4] && watchedFields[4].map((item, index) => (
+                <div key={index}>
+                  <p><strong>Description:</strong> {item.description}</p>
+                  <p><strong>Hours:</strong> {item.hours}</p>
+                  <p><strong>Rate Per Hour:</strong> {item.ratePerHour}</p>
+                </div>
+              ))}
+
+              <p><strong>Tax Rate:</strong> {watchedFields[5]}</p>
+              <p><strong>Terms:</strong> {watchedFields[6]}</p>
+              <p><strong>Payment Details:</strong> {watchedFields[7]}</p>
+            </div>
+        </div>
+
         </div>
       </div>
-    </section>
-  )
+    </div>
+  );
 }
